9f37ceddb7e79dff913e7bb54f6073e42843288d
Christopher Hipper <hipper.christopher@gmail.com>
2025-07-19 12:38:59 +0200

    add Jumping

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,9 @@ class Character extends MovableObject {
     width = 150;
     height = 200;
     y = 230;
+    gravity = 1;
+    jump_y = 0;
+    maxJump = 20;
     keys = [];
     speed = 0;
     maxSpeed = 3;
@@ -11,37 +14,36 @@ class Character extends MovableObject {
     }
     moveCharacter() {
         window.addEventListener('keydown', (e) => {
-            if ((e.key === 'ArrowRight' || e.key === 'ArrowLeft')
+            if ((e.key === 'ArrowRight' || 
+                e.key === 'ArrowLeft'||
+                e.key === 'ArrowUp'
+            )
                 && this.keys.indexOf(e.key) === -1) {
                 this.keys.push(e.key);
             }
-            console.log(e.key, this.keys);
-
         });
         window.addEventListener('keyup', (e) => {
-            if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+            if (e.key === 'ArrowRight' || e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
                 this.keys.splice(this.keys.indexOf(e.key), 1);
             }
-            console.log(e.key, this.keys);
         });
     }
     updateCharacterPosition() {
+        // horizontal movement
         this.x += this.speed;
-        if (this.keys.includes('ArrowRight')) {
-            this.speed = this.maxSpeed;
-        } else if (this.keys.includes('ArrowLeft')) {
-            this.speed = -this.maxSpeed;
-        } else {
-            this.speed = 0;
-        }
-        if (this.x < 0) {
-            this.x = 0;
-        }
-        if (this.x + this.width > canvas.width) {
-            this.x = canvas.width - this.width;
-        }
+        if (this.keys.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (this.keys.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > canvas.width) this.x = canvas.width - this.width;
+        // vertical movement
+        if (this.keys.includes('ArrowUp') && this.onGround()) this.jump_y = -this.maxJump
+        this.y += this.jump_y;
+        if (!this.onGround()) this.jump_y += this.gravity;
+        else this.jump_y = 0;
     }
-    jump() { }
+    onGround() {
+        return this.y >= canvas.height - this.height -50;}
     sleep() { }
     throw() { }
 }
