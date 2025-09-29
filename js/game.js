87cb5a9d06363588de316f7d514a480b555ba6f2
Christopher Hipper <hipper.christopher@gmail.com>
2025-09-29 21:05:10 +0200

    update design

--- js/game.js
+++ js/game.js
@@ -2,13 +2,6 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-function startGame() {
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-    document.getElementById('start-Screen').classList.toggle('d-none');
-    document.getElementById('game-ui').classList.toggle('d-none');
-}
-
 window.addEventListener('keydown', (e) => {
     keyboard.keyPres(e.code)
 })
@@ -17,9 +10,39 @@ window.addEventListener('keyup', (e) => {
     keyboard.keyLeave(e.code)
 })
 
-function toggleControl() {
+function startGame() {
+    canvas = document.getElementById('canvas');
+    world = new World(canvas, keyboard);
+    document.getElementById('start-Screen').classList.toggle('d-none');
+    document.getElementById('game-ui').classList.toggle('d-none');
+}
+
+
+function toggleSettings() {
     document.getElementById('start-Screen').classList.toggle('d-none');
     document.getElementById('settings-screen').classList.toggle('d-none');
+}
+
+function toggleSound() {
+    const soundImg = document.getElementById('mute').src;
+    if (soundImg.indexOf('unmute.png') != -1) {
+        document.getElementById('mute').src = "./assets/img/mute.png"
+    } else {
+        document.getElementById('mute').src = "./assets/img/unmute.png"
+    }
+}
+
+function toggleFullscreen() {
+    const screenImg = document.getElementById('fullscreen').src;
+    const canvas = document.getElementById('canvas')
+    if (screenImg.indexOf('fullscreen.png') != -1) {
+        canvas.webkitRequestFullScreen()
+        canvas.style.borderRadius = '0px';
+        document.getElementById('fullscreen').src = "./assets/img/closeFullscreen.png"
+    } else {
+        document.getElementById('fullscreen').src = "./assets/img/fullscreen.png"
+        canvas.mozRequestFullScreen()
+    }
 }
 
 
