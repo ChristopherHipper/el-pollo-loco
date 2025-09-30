077b5a6b7942464248ab7da3ee10434b4f862c6a
Christopher Hipper <hipper.christopher@gmail.com>
2025-09-30 18:00:51 +0200

    toggle fullscreen

--- js/game.js
+++ js/game.js
@@ -3,47 +3,47 @@ let world;
 let keyboard = new Keyboard();
 
 window.addEventListener('keydown', (e) => {
-    keyboard.keyPres(e.code)
-})
+    keyboard.keyPres(e.code);
+});
 
 window.addEventListener('keyup', (e) => {
-    keyboard.keyLeave(e.code)
-})
+    keyboard.keyLeave(e.code);
+});
 
 function startGame() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
     document.getElementById('start-Screen').classList.toggle('d-none');
     document.getElementById('game-ui').classList.toggle('d-none');
-}
+};
 
 
 function toggleSettings() {
     document.getElementById('start-Screen').classList.toggle('d-none');
     document.getElementById('settings-screen').classList.toggle('d-none');
-}
+};
 
 function toggleSound() {
     const soundImg = document.getElementById('mute').src;
     if (soundImg.indexOf('unmute.png') != -1) {
-        document.getElementById('mute').src = "./assets/img/mute.png"
+        document.getElementById('mute').src = "./assets/img/mute.png";
     } else {
-        document.getElementById('mute').src = "./assets/img/unmute.png"
-    }
-}
+        document.getElementById('mute').src = "./assets/img/unmute.png";
+    };
+};
 
 function toggleFullscreen() {
     const screenImg = document.getElementById('fullscreen').src;
-    const canvas = document.getElementById('canvas')
+    const canvas = document.getElementById('game-content');
     if (screenImg.indexOf('fullscreen.png') != -1) {
-        canvas.webkitRequestFullScreen()
+        canvas.requestFullscreen();
         canvas.style.borderRadius = '0px';
-        document.getElementById('fullscreen').src = "./assets/img/closeFullscreen.png"
+        document.getElementById('fullscreen').src = "./assets/img/closeFullscreen.png";
     } else {
-        document.getElementById('fullscreen').src = "./assets/img/fullscreen.png"
-        canvas.mozRequestFullScreen()
-    }
-}
+        document.getElementById('fullscreen').src = "./assets/img/fullscreen.png";
+        document.exitFullscreen();
+    };
+};
 
 
 
